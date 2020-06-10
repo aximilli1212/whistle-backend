0471a898784c50e6280788391b9abe9f70aea521
Aximilli1212 <aximill1212@gmail.com>
2020-06-10 23:33:32 +0000

    add in bodyparser and cors middlewares

--- index.js
+++ index.js
@@ -6,3 +6,9 @@ const jwt = require('express-jwt');
 const jwks = require('jwks-rsa');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
